435b77817bd2f6f41487bf70d96fd1dbfad536ef
Andrey <andreymaranchak@yandex.ru>
2023-02-22 12:27:00 +0300

    02

--- 02.js
+++ 02.js
@@ -1,8 +1,7 @@
 function countSmileys(arr){
-    const smile = [':)', ':D', ';-D', ':~)', ';~D'];
+    const smile = [':)', ':~D', ':D', ';-D', ':~)', ';~D', ':-D', ':-)', ';D', ';~)', ';-)', ';)'];
     const trueSmiles = arr.filter(e => smile.includes(e));
     return trueSmiles.length;
 }
-
 
 console.log(countSmileys([':)', ';(', ';}', ':-D']));
