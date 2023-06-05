35c31e612a9658e478bd21335bb4da35a1a44638
Endy-Guff <andreymaranchak@yandex.ru>
2023-06-05 16:22:30 +0300

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

---
+++ altERnaTIng cAsE <=> ALTerNAtiNG CaSe.js
@@ -0,0 +1,5 @@
+String.prototype.toAlternatingCase = function () {
+    return this.split('').map(l=>l===l.toUpperCase()?l.toLowerCase():l.toUpperCase()).join('')
+}
+
+console.log('LoL'.toAlternatingCase());
